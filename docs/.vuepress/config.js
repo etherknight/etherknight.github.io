b0445c2a2fa5d4aacc286c3a981a64e8b543e9b5
= <=>
2024-10-13 10:53:36 +0100

    Moved dist output

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,5 +4,20 @@ import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
   bundler: viteBundler(),
-  theme: defaultTheme(),
+  theme: defaultTheme({ 
+    navbar: [
+        {
+            text: 'Home',
+            link: '/',
+        },
+        {
+            text: 'Projects',
+            link: '/projects/'
+        }
+    ]
+  }),
+  dest: "./dist",
+  lang: 'en-GB',
+  title: 'Hello VuePress',
+  description: 'Just playing around',
 })
